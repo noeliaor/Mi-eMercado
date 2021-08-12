53ba5c06b2060ea57966d153acd737ec94216374
noeliaor <noeliaoliverarodriguez@gmail.com>
2021-08-12 16:29:51 -0300

    Autenticación de Google mejorada

--- js/google.js
+++ js/google.js
@@ -15,7 +15,7 @@ function renderButton() {
 function onSuccess(googleUser) {
     // Get the Google profile data (basic)
     //var profile = googleUser.getBasicProfile();
-    
+
     // Retrieve the Google account data
     gapi.client.load('oauth2', 'v2', function () {
         var request = gapi.client.oauth2.userinfo.get({
@@ -23,10 +23,10 @@ function onSuccess(googleUser) {
         });
         request.execute(function (resp) {
             // Display the user details
-            var profileHTML = '<h3>Welcome '+resp.given_name+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></h3>';
-            profileHTML += '<img src="'+resp.picture+'"/><p><b>Google ID: </b>'+resp.id+'</p><p><b>Name: </b>'+resp.name+'</p><p><b>Email: </b>'+resp.email+'</p><p><b>Gender: </b>'+resp.gender+'</p><p><b>Locale: </b>'+resp.locale+'</p><p><b>Google Profile:</b> <a target="_blank" href="'+resp.link+'">click to view profile</a></p>';
+            var profileHTML = '<h3>Bienvenido/a ' + resp.given_name + '! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></h3>';
+            profileHTML += '<img src="' + resp.picture + '"/><p><b>Nombre: </b>' + resp.name + '</p><p><b>Correo electrónico:</b>' + resp.email + '</p><p>';
             document.getElementsByClassName("userContent")[0].innerHTML = profileHTML;
-            
+
             document.getElementById("gSignIn").style.display = "none";
             document.getElementsByClassName("userContent")[0].style.display = "block";
         });
@@ -45,7 +45,18 @@ function signOut() {
         document.getElementsByClassName("userContent")[0].innerHTML = '';
         document.getElementsByClassName("userContent")[0].style.display = "none";
         document.getElementById("gSignIn").style.display = "block";
+        sessionStorage.clear();
     });
-    
+
     auth2.disconnect();
+}
+
+
+var Txtuser = resp.name;
+var Txtpassword = resp.email;
+if (Txtuser && Txtpassword) { //De haber datos de usuario Y contraseña los almacena como objeto de la sesión
+    //redirige al index//página inicial de la tienda
+    sessionStorage.setItem('user', Txtuser);
+    sessionStorage.setItem('password', Txtpassword);
+    window.location.href = "index.html";
 }
