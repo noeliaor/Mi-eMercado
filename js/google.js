b8308ab3ba3ff5061f557431634076d01a7d2d93
noeliaor <noeliaoliverarodriguez@gmail.com>
2021-08-12 20:54:46 -0300

    update google.js+Diagrama.png

--- js/google.js
+++ js/google.js
@@ -10,8 +10,6 @@ function renderButton() {
         'onfailure': onFailure
     });
 
-    var Googleuser = resp.name;
-    var Googlepassword = resp.email;
 
     sessionStorage.setItem('Guser', Googleuser);
     sessionStorage.setItem('Gpassword', Googlepassword);
@@ -32,7 +30,9 @@ function onSuccess(googleUser) {
             // Display the user details
             var profileHTML = '<h3>Bienvenido/a ' + resp.given_name + '!</h3>';
             profileHTML += '<p style="color:red">¡Presiona ingresar!</p>' + '<img src="' + resp.picture + '"/><p><b>Nombre: </b></p><p>' + resp.name + '</p><p><b>Correo electrónico:</b></p><p>' + resp.email + '</p> <a href="javascript:void(0);" onclick="signOut();">Cerrar sesión</a><p>';
-
+            var Googleuser = resp.name;
+            var Googlepassword = resp.email;
+        
             document.getElementsByClassName("userContent")[0].innerHTML = profileHTML;
 
             document.getElementById("gSignIn").style.display = "none";
