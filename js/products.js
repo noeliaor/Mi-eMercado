69ae02d44205e6860352b864136ffb6fecafd656
noeliaor <noeliaoliverarodriguez@gmail.com>
2021-09-02 11:02:01 -0300

    Entrega 2 Final

--- js/products.js
+++ js/products.js
@@ -8,32 +8,41 @@ const showData = (array, SortPreference, concept) => { //Ordena los elementos de
 
   if (SortPreference == "asc") {
     organized = inorder;
-  } else if (SortPreference == "desc") {
-    organized = inorder.reverse(); //En caso de orden descendente revierte el orden de la array
   } else {
-    organized = inorder;
+    organized = inorder.reverse(); //En caso de orden descendente revierte el orden de la array
   }
 
   return organized; //Devuelve la array ordenada
 
 };
 
-const FilterData(DataName,DataDescription,Search)=> {
-  let result=0;
-  if (DataName.search(Search)>0||DataDescription.search(Search)>0){
-    result=1;
-  }
- return result;
+//Desafiate
+const FilterData = (DataName, DataDescription, Search) => { //Esta función busca palabras en dos elementos de información indicados
+  let result = 1; //Inicialmente considero que hay alguna coincidencia
+  let SearchSplit = Search.split(" ") //divido la cadena en palabras para reconocer palabras sueltas
+  for (let item of SearchSplit) { 
+    if (result != 0) { //Si aún no ha descoincidido con la búsqueda
+      if (DataName.toLowerCase().includes(item.toLowerCase()) || DataDescription.toLowerCase().includes(item.toLowerCase())) { //OBS: Las cadenas se convierten a minúscula para que la búsqueda no sea case sensitive;
+        result = 1; //Si alguno de los dos elementos (en este caso descripción y nombre de producto) contiene la búsqueda, retorna 1 (verdadero)
+
+      } else {
+        result = 0; //Si ninguno lo contiene retorna 0 (falso)
+      }
+    }
+  };
+  return result; //Devuelve 1 si todas las palabras se encuentran en descripción y/o nombre, 0 si alguna no se encuentra.
+ 
 };
+//---------------------------------------------------//
 
-
-function showProductsList(array, SortPreference, concept, Minimo, Maximo) {
+function showProductsList(array, SortPreference, concept, Minimo, Maximo, Search) {
   let htmlContentToAppend = ""
   for (let i = 0; i < array.length; i++) {
     let product = showData(array, SortPreference, concept)[i]; //Recibe la array ordenada y la almacena
 
 
-    if (product.cost >= Minimo && product.cost <= Maximo && FilterData()) { //Sólo aquellos productos con precios en el rango definido 
+    if (product.cost >= Minimo && product.cost <= Maximo && FilterData(product.name, product.description, Search) == 1) { //Sólo aquellos productos con precios en el rango definido,
+      //Y que contienen en desc. o nombre la palabra buscada. 
       //se van a mostrar (concatenar al contenido)
       htmlContentToAppend += `
     <div class="list-group-item list-group-item-action">
@@ -64,72 +73,71 @@ function showProductsList(array, SortPreference, concept, Minimo, Maximo) {
 
 document.addEventListener("DOMContentLoaded", async function (e) {
   //Definición de elementos requeridos para la presentación de productos:
-  let minCount = 0; //Inicialmente abarco todos los productos
-  let maxCount = Number.POSITIVE_INFINITY;
+  let minCost = 0; //Inicialmente abarco todos los productos
+  let maxCost = Number.POSITIVE_INFINITY;
   let OrderPreference = "asc"; //Por defecto muestro productos en orden alfabético ascendente
-  let OrderBy = "name";
+  let OrderBy = "cost";
+  let buscador = document.getElementById("buscador");
+  let ToSearch = buscador.value;
   var products = (await getJSONData(PRODUCTS_URL)).data; //Realizo la petición y guardo result.data en una variable
-  showProductsList(products, OrderPreference, OrderBy, minCount, maxCount); //Al ingresar al html se presentan los productos,
+  showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, ToSearch); //Al ingresar al html se presentan los productos,
   //ordenados por defecto
 
 
   document.getElementById("sortAsc").addEventListener("click", function () { //Selección de orden ascendente
     OrderPreference = "asc";
     OrderBy = "cost";
-    showProductsList(products, OrderPreference, OrderBy, minCount, maxCount);
+    showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, ToSearch);
 
   });
   document.getElementById("sortDesc").addEventListener("click", function () { //Selección de orden descendente
     OrderPreference = "desc";
     OrderBy = "cost";
-    showProductsList(products, OrderPreference, OrderBy, minCount, maxCount);
+    showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, ToSearch);
 
   });
 
   document.getElementById("sortByCount").addEventListener("click", function () { //Orden por relevancia
     OrderPreference = "desc";
     OrderBy = "soldCount";
-    showProductsList(products, OrderPreference, OrderBy, minCount, maxCount);
+    showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, ToSearch);
 
   });
 
   //----------------Filtrado por precios--------------
   document.getElementById("clearRangeFilter").addEventListener("click", function () { //Botón de limpieza
-    document.getElementById("rangeFilterCountMin").value = ""; //Vacía campos en los espacios de input
-    document.getElementById("rangeFilterCountMax").value = "";
+    document.getElementById("rangeFilterCostMin").value = ""; //Vacía campos en los espacios de input
+    document.getElementById("rangeFilterCostMax").value = "";
 
-    minCount = 0; //Redefine límites para que abarquen todos los productos
-    maxCount = Number.POSITIVE_INFINITY;
-    showProductsList(products, OrderPreference, OrderBy, minCount, maxCount); //Aplico nuevos límites
+    minCost = 0; //Redefine límites para que abarquen todos los productos
+    maxCost = Number.POSITIVE_INFINITY;
+    showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, ToSearch); //Aplico nuevos límites
   });
 
-  document.getElementById("rangeFilterCount").addEventListener("click", function () { //Selección de filtrado
+  document.getElementById("rangeFilterCost").addEventListener("click", function () { //Selección de filtrado
     //Obtengo el mínimo y máximo de los intervalos para filtrar por precio
-    minCount = document.getElementById("rangeFilterCountMin").value;
-    maxCount = document.getElementById("rangeFilterCountMax").value;
+    minCost = document.getElementById("rangeFilterCostMin").value;
+    maxCost = document.getElementById("rangeFilterCostMax").value;
 
-    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0) { //Si hay un mínimo, razonable, definido lo almacena como número
-      minCount = parseInt(minCount);
+    if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0) { //Si hay un mínimo, razonable, definido lo almacena como número
+      minCost = parseInt(minCost);
     }
     else {
-      minCount = 0; //Defino mínimo como el mínimo posible
+      minCost = 0; //Defino mínimo como el mínimo posible
     }
 
-    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0) { //Idem para el máximo
-      maxCount = parseInt(maxCount);
+    if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0) { //Idem para el máximo
+      maxCost = parseInt(maxCost);
     }
     else {
-      maxCount = Number.POSITIVE_INFINITY; //Defino máximo inalcanzable
+      maxCost = Number.POSITIVE_INFINITY; //Defino máximo inalcanzable
     }
-    showProductsList(products, OrderPreference, OrderBy, minCount, maxCount); //Vuelve a mostrar la lista
+    showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, ToSearch); //Vuelve a mostrar la lista
   });
-});
-//Desafiate
 
-let buscador = document.getElementById("buscador");
-buscador.addEventListener("keydown", (event) => {
-  let ToSearch = buscador.value;
-  alert(ToSearch)
-  showProductsList(products, OrderPreference, OrderBy, minCount, maxCount, ToSearch);
+  //Desafiate
+  buscador.addEventListener("keyup", (event) => { //Cada vez que se presiona y suelta una tecla en el input para búsquedas
+    showProductsList(products, OrderPreference, OrderBy, minCost, maxCost, buscador.value); //Realiza la muestra de productos con la cadena a buscar
+});
 });
 
